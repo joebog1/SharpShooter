faf10407df499f32b17c2a02cd0bf0dde35c85be
joebog1 <joebog96@gmail.com>
2026-08-19 20:29:00 +0930

    Added UCI tests.

---
+++ Tests/UCITests.cs
@@ -0,0 +1,18 @@
+﻿using SharpShooter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+  public class UCITests
+  {
+    [Fact]
+    public void Engine_AsksIsReady_returnsReadyOk()
+    {
+      var engine = new ChessEngine("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+
+    }
+  }
+}
